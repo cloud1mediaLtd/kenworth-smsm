bd252a0c225069abc4ce2b044a5497ffc20cd49a
Anas Mahmud <admin@cloud1media.com>
2024-05-12 19:35:13 +0100

    update company name

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -40,13 +40,13 @@ export default async function aboutPage() {
           <div>
             <h1>Welcome</h1>
             <p>
-              As the official dealer of Dodge, Chrysler, Jeep, Ram, Fiat, Peugeot, and Mopar in Libya, S.Mediterranean Motors is proud to offer a wide range of new vehicles to meet your diverse needs. Our dealership provides more than just quality vehicles; we offer a full suite of services, including maintenance, repairs, and genuine spare parts, ensuring your vehicle performs at its best.
+              As the official dealer of Kenworth in Libya, Gasos Libya is proud to offer a wide range of new vehicles to meet your diverse needs. Our dealership provides more than just quality vehicles; we offer a full suite of services, including maintenance, repairs, and genuine spare parts, ensuring your vehicle performs at its best.
             </p>
             <p>
               Our dedicated team of professionals is here to guide you through every step of your automotive journey, whether you're purchasing a new vehicle or need expert care for your existing one. With a focus on exceptional customer service, we strive to build lasting relationships with our clients, providing reliable support and comprehensive solutions for all your automotive needs.
             </p>
             <p>
-              We invite you to explore our extensive vehicle lineup and discover the unparalleled services that make S.Mediterranean Motors a trusted partner on the road.
+              We invite you to explore our extensive vehicle lineup and discover the unparalleled services that make Gasos Libya a trusted partner on the road.
             </p>
           </div>
 
@@ -60,13 +60,13 @@ export default async function aboutPage() {
           <div>
             <h1>Mission and Vision Statements</h1>
             <p>
-              At S.Mediterranean Motors, our mission is to redefine the automotive experience in Libya by providing unparalleled access to globally renowned brands. As the official dealer of Dodge, Chrysler, Jeep, Ram, Fiat, Peugeot, and Mopar, we are proud to represent these iconic names for the first time in Libya, except Fiat, which last operated in the country before the 1970s, and Peugeot, reintroduced in 2014.
+              At Gasos Libya, our mission is to redefine the automotive experience in Libya by providing unparalleled access to globally renowned brands. As the official dealer of Dodge, Chrysler, Jeep, Ram, Fiat, Peugeot, and Mopar, we are proud to represent these iconic names for the first time in Libya, except Fiat, which last operated in the country before the 1970s, and Peugeot, reintroduced in 2014.
             </p>
             <p>
               We are committed to offering an exceptional range of new vehicles, along with comprehensive after-sales services such as maintenance, repairs, and genuine spare parts. By focusing on quality and customer satisfaction, we aim to build lasting relationships and be a trusted partner for all your automotive needs.
             </p>
             <p>
-              S.Mediterranean Motors is here to deliver an automotive experience that reflects the reliability and innovation of our brands while ensuring that our clients receive personalized support and expertise every step of the way.
+              Gasos Libya is here to deliver an automotive experience that reflects the reliability and innovation of our brands while ensuring that our clients receive personalized support and expertise every step of the way.
             </p>
           </div>
 
@@ -80,7 +80,7 @@ export default async function aboutPage() {
           <div>
             <h1>Customer Testimonials</h1>
             <p>
-              At S.Mediterranean Motors, our customers' satisfaction is at the heart of everything we do. We are proud to have earned the trust and loyalty of numerous esteemed companies and brands that rely on our vehicles and services.
+              At Gasos Libya, our customers' satisfaction is at the heart of everything we do. We are proud to have earned the trust and loyalty of numerous esteemed companies and brands that rely on our vehicles and services.
             </p>
 
             <div className='grid grid-cols-3 gap-4 pt-6'>
@@ -97,7 +97,7 @@ export default async function aboutPage() {
           <div>
             <h1>Community Involvement</h1>
             <p>
-              At S.Mediterranean Motors, we believe in driving positive change within our community. Our commitment to community involvement goes beyond offering quality vehicles and services. We take pride in being the official gold sponsor of the Libya Rally, supporting this adventurous and iconic event that embodies the spirit of exploration. Additionally, we are honored to be the official sponsor of the Libyan Jeep Enthusiast Club, where we celebrate the passion and camaraderie of Jeep lovers across the country.
+              At Gasos Libya, we believe in driving positive change within our community. Our commitment to community involvement goes beyond offering quality vehicles and services. We take pride in being the official gold sponsor of the Libya Rally, supporting this adventurous and iconic event that embodies the spirit of exploration. Additionally, we are honored to be the official sponsor of the Libyan Jeep Enthusiast Club, where we celebrate the passion and camaraderie of Jeep lovers across the country.
             </p>
 
             <p>
@@ -118,7 +118,7 @@ export default async function aboutPage() {
           <div className='pt-4'>
             <h1>Gallery or Virtual Tour</h1>
             <p>
-              Explore the world of S.Mediterranean Motors through our Gallery and Virtual Tour, where you can discover our impressive selection of Dodge, Chrysler, Jeep, Ram, Fiat, Peugeot, and Mopar vehicles. Get a close-up view of their innovative features and meticulous craftsmanship, and take a virtual stroll through our showroom to find the perfect vehicle that matches your style and needs.            </p>
+              Explore the world of Gasos Libya through our Gallery and Virtual Tour, where you can discover our impressive selection of Dodge, Chrysler, Jeep, Ram, Fiat, Peugeot, and Mopar vehicles. Get a close-up view of their innovative features and meticulous craftsmanship, and take a virtual stroll through our showroom to find the perfect vehicle that matches your style and needs.            </p>
             <div>
               <Card className='flex items-center justify-center h-64 mt-4' >Logo Here</Card>
             </div>
@@ -127,7 +127,7 @@ export default async function aboutPage() {
           <div>
             <h1>News and Events</h1>
             <p>
-              Stay up to date with the latest at S.Mediterranean Motors through our News and Events section. Here, you'll find exciting announcements, insightful industry updates, and information about upcoming events. Whether it's a thrilling rally, an exclusive vehicle launch, or a community gathering, this is your hub for all things related to Dodge, Chrysler, Jeep, Ram, Fiat, Peugeot, and Mopar in Libya.
+              Stay up to date with the latest at Gasos Libya through our News and Events section. Here, you'll find exciting announcements, insightful industry updates, and information about upcoming events. Whether it's a thrilling rally, an exclusive vehicle launch, or a community gathering, this is your hub for all things related to Dodge, Chrysler, Jeep, Ram, Fiat, Peugeot, and Mopar in Libya.
             </p>
             {stories ? (
               <PostsPreview stories={stories} />
