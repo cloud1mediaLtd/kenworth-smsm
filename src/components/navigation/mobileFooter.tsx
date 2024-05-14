fa39959685b99bc9cc5df70750ea0334f4d56ce0
Anas Mahmud <admin@cloud1media.com>
2024-05-14 22:47:20 +0100

    updated mobile footer

--- src/components/navigation/mobileFooter.tsx
+++ src/components/navigation/mobileFooter.tsx
@@ -1,3 +1,4 @@
+import { Button } from "components/ui/button";
 import { PhoneIcon } from "lucide-react";
 import Link from "next/link";
 
@@ -8,14 +9,14 @@ export default function MobileFooter({ isRtl }) {
         <footer className="flex sm:hidden shadow-lg h-18 border-t" dir="ltr">
             <div className="flex justify-between items-center w-full bg-black">
                 <div className=" px-5 w-full">
-                    <span className="flex justify-center w-full">
+                    <Button variant="secondary" className="flex justify-center w-full">
                         <Link
                             href="/quote/schedule"
-                            className="bg-slate-50 hover:bg-orange-600 p-2 w-full rounded-md text-small-semi text-center hover:underline underline-offset-2 shadow-sm"
+                            className=" p-2 w-full rounded-md text-small-semi text-center hover:underline underline-offset-2 shadow-sm"
                         >
                             {isRtl ? (<span>اتصال</span>) : (<span>Contact</span>)}
                         </Link>
-                    </span>
+                    </Button>
                 </div>
 
                 <div className="flex gap-3 items-center justify-center bg-green-700 p-4 text-white h-18 w-18">
