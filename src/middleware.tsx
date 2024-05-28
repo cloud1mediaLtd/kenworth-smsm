d27bcfeabbeaed2d938f4ea42ea28f76e8f739e5
Anas Mahmud <admin@cloud1media.com>
2024-05-28 03:15:26 +0100

    UPDATE MIDDLE WARE

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -15,10 +15,12 @@ export const config = {
 
     // Set a cookie to remember the previous locale for
     // all requests that have a locale prefix
-    '/(de|en)/:path*',
+    '/(ar|en)/:path*',
 
     // Enable redirects that add missing locales
     // (e.g. `/pathnames` -> `/en/pathnames`)
-    '/((?!_next|_vercel|.*\\..*).*)'
+    '/((?!api|_next|.*\\..*).*)'
   ]
 };
+
+// matcher: ['/((?!api|_next|.*\\..*).*)']
